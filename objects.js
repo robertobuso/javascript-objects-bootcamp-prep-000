c42539ea9125a15b5a7981a483cdc79c1b0a8fd0
Roberto Busó-García <robertobuso@gmail.com>
2018-06-30 14:33:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,10 @@ function updatePlaylist (playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist (playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName];
   return playlist;
+=======
+ delete artistName.playlist
+>>>>>>> 6efdf01f0b663d8467ed65e1571e9f126ff47bc1
 }
